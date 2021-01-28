eaaf9a0a2427479e3496effd835a966ce9c39c86
muhammed hakan tekbaş <muhammedhakan.tekbas@agu.edu.tr>
2021-01-28 21:17:11 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi  ");
+        }
+
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listelendi  ");
+        }
+
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silindi  ");
+        }
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri musteri1 = new Musteri();
+            Musteri musteri2 = new Musteri();
+            Musteri musteri3 = new Musteri();
+
+
+            musteri1.Id = 1000;
+            musteri1.Name = "Mustafa";
+            musteri1.LastName = "Aksoy";
+
+            musteri2.Id = 1000;
+            musteri2.Name = "Mustafa";
+            musteri2.LastName = "Aksoy"; 
+            
+            musteri3.Id = 1000;
+            musteri3.Name = "Mustafa";
+            musteri3.LastName = "Aksoy";
+
+
+
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+            musteriManager.Add(musteri3);
+
+            Musteri[] Musteriler = new Musteri[] {musteri1, musteri2, musteri3};
+            foreach (Musteri musteri in Musteriler)
+            {
+                musteriManager.List(musteri);
+            }
+
+            musteriManager.Delete(musteri1);
+            musteriManager.Delete(musteri2);
+
+        }
+
+
+        
+    }
+
+}
